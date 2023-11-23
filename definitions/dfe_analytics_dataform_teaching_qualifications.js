17cba279163a6110f3a50c34bb2808bed6e3e506
Noelyn  Onah <noelyn.onah@digital.education.gov.uk>
2023-11-23 00:37:40 +0000

    added search log table to access result_count

--- definitions/dfe_analytics_dataform_teaching_qualifications.js
+++ definitions/dfe_analytics_dataform_teaching_qualifications.js
@@ -1,12 +1,35 @@
 const dfeAnalyticsDataform = require("dfe-analytics-dataform");
 
 dfeAnalyticsDataform({
-  eventSourceName: "teaching_qualifications",
-  bqProjectName: "teaching-qualifications",
-  bqDatasetName: "events_production",
-  bqEventsTableName: "events",
-  bqEventsTableNameSpace: "access-your-teaching-qualifications",
-  urlRegex: "access-your-teaching-qualifications.education.gov.uk",
-  transformEntityEvents: false,
-  dataSchema: []
+    eventSourceName: "teaching_qualifications",
+    bqProjectName: "teaching-qualifications",
+    bqDatasetName: "events_production",
+    bqEventsTableName: "events",
+    bqEventsTableNameSpace: "access-your-teaching-qualifications",
+    urlRegex: "access-your-teaching-qualifications.education.gov.uk",
+    dataSchema: [{
+        entityTableName: "search_logs",
+        description: "",
+        keys: [{
+                keyName: "dsi_user_id",
+                dataType: "integer",
+                description: "",
+            },
+            {
+                keyName: "last_name",
+                dataType: "string",
+                description: "",
+            },
+            {
+                keyName: "date_of_birth",
+                dataType: "string",
+                description: "",
+            },
+            {
+                keyName: "result_count",
+                dataType: "string",
+                description: "",
+            },
+        ],
+    }, ],
 });
