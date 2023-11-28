ada486fa2777dab8c819ed2e712dc95a625f05bf
Noelyn  Onah <noelyn.onah@digital.education.gov.uk>
2023-11-28 14:54:50 +0000

    added result_count

--- definitions/dfe_analytics_dataform_check.js
+++ definitions/dfe_analytics_dataform_check.js
@@ -1,12 +1,35 @@
 const dfeAnalyticsDataform = require("dfe-analytics-dataform");
 
 dfeAnalyticsDataform({
-  eventSourceName: "check_record",
-  bqProjectName: "teaching-qualifications",
-  bqDatasetName: "events_production",
-  bqEventsTableName: "events_production.events",
-  bqEventsTableNameSpace: "check-the-record-of-a-teacher",
-  urlRegex: "check-the-record-of-a-teacher.education.gov.uk",
-  transformEntityEvents: false,
-  dataSchema: []
+    eventSourceName: "check_record",
+    bqProjectName: "teaching-qualifications",
+    bqDatasetName: "events_production",
+    bqEventsTableName: "events",
+    bqEventsTableNameSpace: "check-the-record-of-a-teacher",
+    urlRegex: "check-the-record-of-a-teacher.education.gov.uk",
+    dataSchema: [{
+        entityTableName: "search_logs",
+        description: "",
+        keys: [{
+                keyName: "dsi_user_id",
+                dataType: "integer",
+                description: "",
+            },
+            {
+                keyName: "last_name",
+                dataType: "string",
+                description: "",
+            },
+            {
+                keyName: "date_of_birth",
+                dataType: "string",
+                description: "",
+            },
+            {
+                keyName: "result_count",
+                dataType: "string",
+                description: "",
+            },
+        ],
+    }, ],
 });
--- definitions/dfe_analytics_dataform_teaching_qualifications.js
+++ definitions/dfe_analytics_dataform_teaching_qualifications.js
@@ -7,29 +7,6 @@ dfeAnalyticsDataform({
     bqEventsTableName: "events",
     bqEventsTableNameSpace: "access-your-teaching-qualifications",
     urlRegex: "access-your-teaching-qualifications.education.gov.uk",
-    dataSchema: [{
-        entityTableName: "search_logs",
-        description: "",
-        keys: [{
-                keyName: "dsi_user_id",
-                dataType: "integer",
-                description: "",
-            },
-            {
-                keyName: "last_name",
-                dataType: "string",
-                description: "",
-            },
-            {
-                keyName: "date_of_birth",
-                dataType: "string",
-                description: "",
-            },
-            {
-                keyName: "result_count",
-                dataType: "string",
-                description: "",
-            },
-        ],
-    }, ],
+    transformEntityEvents: false,
+    dataSchema: []
 });
