91e1b2911a7ee79dc665cfa4941b9a52a1998822
Noelyn  Onah <noelyn.onah@digital.education.gov.uk>
2023-11-29 16:57:07 +0000

    updated names of file

--- definitions/dfe_analytics_dataform_ccbl.js
+++ definitions/dfe_analytics_dataform_ccbl.js
@@ -1,7 +1,7 @@
 const dfeAnalyticsDataform = require("dfe-analytics-dataform");
 
 dfeAnalyticsDataform({
-  eventSourceName: "check_child_barred_list",
+  eventSourceName: "ccbl",
   bqProjectName: "teaching-qualifications",
   bqDatasetName: "ccbl_events_production",
   bqEventsTableName: "events",
